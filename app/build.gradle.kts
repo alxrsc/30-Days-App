8ff49a170f13ee910f89f5a354bd26c1a268ab06
Alexandru Rosca <alexandrurosca968@gmail.com>
2025-08-10 19:25:38 +0300

    Added material theme and got it working :D

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.4.3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
